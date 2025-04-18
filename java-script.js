2c4b53f19254bb96c171a4d8b1dfd7460daf507d
Varun <varunreddy51155@gmail.com>
2025-04-18 16:54:41 +0530

    java-script.js is modified

---
+++ java-script.js
@@ -0,0 +1,16 @@
+let name = "varun"
+let age = 20
+let isalive = true
+let score
+let data = null
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isalive);
+console.log(typeof score);
+console.log(typeof data);
+
+
+
+
+
