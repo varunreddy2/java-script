0477fbd3c623ec5ee083272abfb2435e2ee9b354
Varun <varunreddy51155@gmail.com>
2025-04-18 17:31:46 +0530

    operators.js file is modified

---
+++ operators.js
@@ -0,0 +1,26 @@
+let a = 10
+let b = 3
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log(a % b);
+console.log(a ** b);
+// comparision operators
+console.log(a == b);
+console.log(a === b);
+console.log(a > b);
+console.log(a < b);
+// logical operators
+console.log(a > b && b < a);
+console.log(a < b);
+
+
+
+
+
+
+
+
+
