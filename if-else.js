005dec330ec3a5dd99f6ddbb70101d100d250224
Varun <varunreddy51155@gmail.com>
2025-04-22 19:19:28 +0530

    if-else.js is modified

--- if-else.js
+++ if-else.js
@@ -9,11 +9,15 @@ if (marks >= 90) {
 } else if (marks >= 50) {
     console.log("average");
 
-} else  if(marks >= 35) {
+} else if (marks >= 35) {
     console.log("passed");
 
 } else {
     console.log("fail");
-    
+
 }
+
+let resluts = marks >= 35 ? "pass" : "fail"
+console.log(resluts);
+
 
